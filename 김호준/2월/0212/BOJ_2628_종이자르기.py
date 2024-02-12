360e722625b5c91e891b5b8086da1853403d9c66
hojun-kim <156287257+ssafy11@users.noreply.github.com>
2024-02-12 21:41:59 +0900

    Update BOJ_2628_종이자르기.py

--- 김호준/2월/0212/BOJ_2628_종이자르기.py
+++ 김호준/2월/0212/BOJ_2628_종이자르기.py
@@ -1,24 +1,24 @@
 G, S = map(int, input().split())
 T = int(input())
 
-G_arr = [0, G]
-S_arr = [0, S]
+G_arr = [0, G] # 가로 그룹 [0, 10, 4]
+S_arr = [0, S] # 세로 그룹 [0, 8, 3, 2]
 for _ in range(T):
     N, M = map(int, input().split())
-    if N == 1:
-        G_arr.append(M)
+    if N == 1: # 여기서 반대로 했다가 애먹음
+        G_arr.append(M) # 앞에 1이면 세로로 자르니까 가로길이 나눠짐
     else:
-        S_arr.append(M)
+        S_arr.append(M) # 0이면 가로로 자름 세로길이 나눠짐
 
-G_arr.sort()
-S_arr.sort()
+G_arr.sort() # 정렬시켜줌 [0, 4, 10]
+S_arr.sort() # [0, 2, 3, 8]
 
-G_result = 0
+G_result = 0 # 가로 최대값
 for i in range(len(G_arr)-1):
-    if G_result < G_arr[i+1] - G_arr[i]:
-        G_result = G_arr[i+1] - G_arr[i]
+    if G_result < G_arr[i+1] - G_arr[i]: # 뒤에꺼에서 앞에꺼 뺌
+        G_result = G_arr[i+1] - G_arr[i] # 그럼 길이 나옴
         
-S_result = 0
+S_result = 0 # 세로 최대값
 for j in range(len(S_arr)-1):
     if S_result < S_arr[j+1] - S_arr[j]:
         S_result = S_arr[j+1] - S_arr[j]
