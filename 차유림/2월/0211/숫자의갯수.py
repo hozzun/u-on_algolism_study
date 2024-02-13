5f3cff8a7830630d7dbacff28e6df9bc819043df
chajoyhoi <156291521+chajoyhoi@users.noreply.github.com>
2024-02-13 22:20:51 +0900

    Update 숫자의갯수.py

--- 차유림/2월/0211/숫자의갯수.py
+++ 차유림/2월/0211/숫자의갯수.py
@@ -1,10 +1,10 @@
-A = int(input())
+A = int(input()) 
 B = int(input())
 C = int(input())
 num = A * B * C 
-num1 = list(map(int,str(num)))
-counts = [0] * 10
+num1 = list(map(int,str(num))) # 하나하나 따로 해주기 위해서 map사용 
+counts = [0] * 10 #counts 리스트 만들어서 해당 숫자 있으면 1 추가 
 for i in num1:
   counts[i] += 1
-for j in counts:
+for j in counts: # counts 리스트 나열 
   print(j)
