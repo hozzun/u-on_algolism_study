1a507d1b8c7cfed9db4b5a912eb062447af41292
Suin Hong <78659190+ssuinh@users.noreply.github.com>
2024-02-17 04:04:16 +0900

    Create [실버4]BOJ_2980_도로와 신호등.py

---
+++ 홍수인/2월/0209/[실버4]BOJ_2980_도로와 신호등.py
@@ -0,0 +1,17 @@
+N, L = list(map(int, input().split()))
+
+result = pre_D = 0   # 이동하는데 걸리는 시간 총 합
+for i in range(N):
+    D, R, G = list(map(int, input().split()))
+    
+    result += D - pre_D
+    if result % (R + G) < R or result % (R + G) == 0:  # 빨간불일 경우
+        result += R-(result % (R + G))
+    else: # 초록불일 경우
+        pass
+    
+    pre_D = D
+    
+result += L - D
+    
+print(result)
