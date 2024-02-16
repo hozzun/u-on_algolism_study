904ac4c8bafeedcedc6e86a3b0973036d52ab6c6
Suin Hong <78659190+ssuinh@users.noreply.github.com>
2024-02-17 04:37:46 +0900

    Update [실버4]BOJ_2980_도로와 신호등.py

--- 홍수인/2월/0209/[실버4]BOJ_2980_도로와 신호등.py
+++ 홍수인/2월/0209/[실버4]BOJ_2980_도로와 신호등.py
@@ -5,13 +5,12 @@ for i in range(N):
     D, R, G = list(map(int, input().split()))
     
     result += D - pre_D
-    if result % (R + G) < R or result % (R + G) == 0:  # 빨간불일 경우
+    if result % (R + G) < R:  # 빨간불일 경우
         result += R-(result % (R + G))
     else: # 초록불일 경우
         pass
     
     pre_D = D
-    
-result += L - D
-    
+
+result += L - D  # 마지막 신호등 ~ 종점
 print(result)
