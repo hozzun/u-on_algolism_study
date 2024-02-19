0de08c7633caf7e94eed0af9cc59b01efc3c23d4
undertheear <155411126+undertheear@users.noreply.github.com>
2024-02-19 17:25:46 +0900

    Create [실5]BOJ_10773_제로.py

---
+++ 김의태/2월/0219/[실5]BOJ_10773_제로.py
@@ -0,0 +1,9 @@
+k = int(input())
+k_list = []
+for i in range(k): #스택...0이면 빼고 아니면 넣고..
+    num = int(input())
+    if num != 0:
+        k_list.append(num)
+    else:
+        k_list.pop()
+print(sum(k_list))
