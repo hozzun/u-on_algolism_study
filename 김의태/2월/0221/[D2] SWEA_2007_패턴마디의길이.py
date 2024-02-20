dcfdb643233fb44f4c539f5713f9d4ba4092ddf1
undertheear <155411126+undertheear@users.noreply.github.com>
2024-02-20 21:01:24 +0900

    Create [D2] SWEA_2007_패턴마디의길이.py

---
+++ 김의태/2월/0221/[D2] SWEA_2007_패턴마디의길이.py
@@ -0,0 +1,7 @@
+t = int(input())
+for k in range(0, t):
+    string = input()
+    for i in range(1, 11): #최대 마디는 10마디이니까 그냥 무식하게 슬라이싱
+        if string[:i] == string[i:2*i]: #두배 간격으로 같으면 정답
+            print(f'#{k+1} {i}')
+            break
