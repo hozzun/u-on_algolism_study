ea95d886215146319acc975971f500c15fbbb282
JinAyeong <156287371+JinAyeong@users.noreply.github.com>
2024-02-26 17:39:27 +0900

    Create [D3]SWEA_2085_농작물수확하기.py

---
+++ 진아영/2월/0226/[D3]SWEA_2085_농작물수확하기.py
@@ -0,0 +1,26 @@
+for tc in range(1, 1+int(input())):
+    N = int(input())
+    farm = [list(map(int, input())) for _ in range(N)]
+    crops = 0
+
+    for i in range(N // 2 + 1):
+
+        # 농작물의 한 가운데를 지나가는 중이면
+        if i == N // 2:
+            crops += farm[i][N // 2]
+            for j in range(1, i + 1):
+                crops += farm[i][N // 2 - j]
+                crops += farm[i][N // 2 + j]
+
+        # 그렇지 않으면 위아래로 더해줌
+        else:
+            crops += farm[i][N//2]
+            crops += farm[N - i - 1][N // 2]          # 여기
+
+            for j in range(1, i+1):
+                crops += farm[i][N//2 - j]
+                crops += farm[i][N//2 + j]
+                crops += farm[N - i -1][N // 2 - j]   # 여기
+                crops += farm[N - i -1][N // 2 + j]   # 여기가 아랫부분
+
+    print(f'#{tc} {crops}')
