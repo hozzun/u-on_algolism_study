0b05c7f8e5b4f65ec86d1b45a76b89be5eb17f5a
chajoyhoi <156291521+chajoyhoi@users.noreply.github.com>
2024-03-01 23:32:49 +0900

    Update [실4]BOJ_2578_빙고.py

--- 차유림/2월/0227/[실4]BOJ_2578_빙고.py
+++ 차유림/2월/0227/[실4]BOJ_2578_빙고.py
@@ -41,4 +41,43 @@ def check(arr1, arr2):
 
 result = check(bingo_lst, mc) 
 print(result)
+
+
+# -------------------------------------------------
+bingo_lst = [list(map(int, input().split())) for _ in range(5)]
+mc = [list(map(int, input().split())) for _ in range(5)]
+cnt = 0
+def check(arr1, arr2):   
+    global cnt
+    di = [0, 1, 1, 1]
+    dj = [1, 0, 1, -1]  
+    for i in range(5):
+        for j in range(5):
+            delete_v = arr2[i][j]
+            for r in range(5):
+                for c in range(5):
+                    if arr1[r][c] == delete_v:
+                        cnt += 1
+                        arr1[r][c] = 0
+                        break
+            bingo_cnt = 0 
+            for k in range(4):
+                bingo1 = 0
+                for l in range(5):
+                    ni = i + di[k] * l
+                    nj = j + dj[k] * l
+                    if not (0 <= ni < 5 and 0 <= nj < 5):
+                        break 
+                    if arr1[ni][nj] == 0:
+                        bingo1 += 1
+                if bingo1 == 5:
+                    bingo_cnt += 1
+            if bingo_cnt >= 3:
+                return cnt
             
+  
+
+result = check(bingo_lst, mc) 
+print(result)
+
+뭔가 안돼 ㅜ ㅜ///..
