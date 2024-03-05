8fd1f92670b8b80b7e4acb0bec25a5d992d42f6d
JinAyeong <156287371+JinAyeong@users.noreply.github.com>
2024-03-05 16:50:13 +0900

    Update [실2]BOJ_4963_섬의개수.py

--- 진아영/3월/0307/[실2]BOJ_4963_섬의개수.py
+++ 진아영/3월/0307/[실2]BOJ_4963_섬의개수.py
@@ -1,33 +1,33 @@
+# 117716 KB, 192 ms
 
-def bfs(i, j):
-    visited[i][j] = 1  # 섬 방문표시
-    q = [(i, j)]  # 큐 초기화
 
-    while q:  # 큐가 비어있을 때까지 반복
-        r, c = q.pop(0)  # 큐에서 섬의 위치를 가져옴
+def dfs(i, j):
+    visited[i][j] = 1
 
-        for n in range(8):  # 섬 주변 탐색
-            ni = r + di[n]
-            nj = c + dj[n]
-            if 0 <= ni < H and 0 <= nj < W and sea_map[ni][nj] and not visited[ni][nj]:  # 그 주위도 땅이라면
-                visited[ni][nj] = 1  # 방문 표시
-                q.append((ni, nj))   # 탐색하기 위해 큐에 저장
+    for n in range(8):   # 주위 8구역 땅인지 탐색
+        ni = i + di[n]
+        nj = j + dj[n]
+        if 0 <= ni < H and 0 <= nj < W and not visited[ni][nj] and sea_map[ni][nj]:   # 방문하지 않은 곳이고 땅이면
+            dfs(ni, nj)
 
 
 while True:
-    W, H = map(int, input().split())  # 너비 W, 높이 H
-    if not W and not H:
+
+    try:
+        W, H = map(int, input().split())
+        sea_map = [list(map(int, input().split())) for _ in range(H)]
+        visited = [[0] * W for _ in range(H)]
+        island = 0
+        di = [1, 1, 1, 0, 0, -1, -1, -1]
+        dj = [-1, 0, 1, -1, 1, -1, 0, 1]
+
+        if W or H:     # w랑 h 0인 이상한 테케 주어지길래 이렇게 함
+            for r in range(H):
+                for c in range(W):
+                    if not visited[r][c] and sea_map[r][c]:    # 지도 다 보면서 땅인곳 깊이우선탐색
+                        dfs(r, c)
+                        island += 1
+            print(island)
+
+    except EOFError:   # 테케 수 주어지지 않았을 때 인풋 없으면 예외처리
         break
-
-    sea_map = [list(map(int, input().split())) for _ in range(H)]
-    visited = [[0] * W for _ in range(H)]  # 각 테스트 케이스마다 visited 배열 초기화
-    di = [1, 1, 1, 0, 0, -1, -1, -1]
-    dj = [-1, 0, 1, -1, 1, -1, 0, 1]
-
-    island = 0
-    for r in range(H):
-        for c in range(W):
-            if sea_map[r][c] and not visited[r][c]:   # 지도를 전부 확인하면서 땅이면서 방문안한곳 깊이우선탐색
-                island += 1
-                bfs(r, c)
-    print(island)
