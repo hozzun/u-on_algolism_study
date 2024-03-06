9fc0e83d530383c7bd558865e63ceeee59bf9d45
chajoyhoi <156291521+chajoyhoi@users.noreply.github.com>
2024-03-06 17:53:53 +0900

    Update [실2]BOJ_11724_연결요소의개수.py

--- 차유림/3월/0306/[실2]연결요소의개수.py
+++ 차유림/3월/0306/[실2]연결요소의개수.py
@@ -1,1 +1,23 @@
-아직안품
+# 백준 11724번 연결 요소의 개수
+# 메모리 : 173484KB 시간 476ms 길이: 433B
+N, M = map(int, input().split())  # N: 정점의 갯수 M : 간선의 갯수
+arr = [[] for _ in range(N + 1)]
+visited = [0] * (N + 1)
+cnt = 0
+for _ in range(M):
+    u, v = map(int, input().split())
+    arr[u].append(v)
+    arr[v].append(u)
+
+def dfs(i):
+    visited[i] = 1
+    for w in arr[i]:
+        if visited[w] == 0:
+            dfs(w)
+
+for i in range(1, N+1):
+    if visited[i] == 0:
+        dfs(i)
+        cnt += 1
+
+print(cnt)
