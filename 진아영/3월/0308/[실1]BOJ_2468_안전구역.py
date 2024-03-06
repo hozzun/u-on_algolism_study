15e11b1fa9ed8531bc0aba9d54d0b5b81846bea7
JinAyeong <156287371+JinAyeong@users.noreply.github.com>
2024-03-06 17:42:57 +0900

    Update [실1]BOJ_2468_안전구역.py

--- 진아영/3월/0308/[실1]BOJ_2468_안전구역.py
+++ 진아영/3월/0308/[실1]BOJ_2468_안전구역.py
@@ -1,4 +1,45 @@
-# 메모리초과로 실패 -> 집가서 수정해보려고 여기 일단 저장~
+# 116044 KB, 276 ms
+
+from collections import deque
+
+def bfs(i, j, h):
+    q = deque()
+    visited[i][j] = 1
+    q.append((i, j))
+
+    while q:
+        a = q.popleft()
+        for n in range(4):
+            ni = a[0] + di[n]
+            nj = a[1] + dj[n]
+            if 0 <= ni < N and 0 <= nj < N and not visited[ni][nj] and ground[ni][nj] > h:
+                q.append((ni, nj))
+                visited[ni][nj] = 1
+
+
+N = int(input())
+ground = [list(map(int, input().split())) for _ in range(N)]
+max_safe_area = 0
+max_high = max(map(max, ground))
+di = [0, 0, -1, 1]
+dj = [-1, 1, 0, 0]
+
+
+for high in range(0, max_high + 1):
+    visited = [[0] * N for _ in range(N)]
+    safe_area = 0
+    for r in range(N):
+        for c in range(N):
+            if ground[r][c] > high and not visited[r][c]:
+                bfs(r, c, high)
+                safe_area += 1
+    if safe_area > max_safe_area:
+        max_safe_area = safe_area
+
+print(max_safe_area)
+
+
+#------------------------------------메모리초과로 실패 -> 집가서 수정해보려고 여기 일단 저장~------------------------------------
 
 import sys
 sys.setrecursionlimit(100000)
