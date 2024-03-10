65b3d4ca7e83c4c2f8595b4856162329253695e1
chajoyhoi <156291521+chajoyhoi@users.noreply.github.com>
2024-03-10 17:07:53 +0900

    Update [실2]BOJ_4963_섬의개수.py

--- 차유림/3월/0307/[실2]BOJ_4963_섬의개수.py
+++ 차유림/3월/0307/[실2]BOJ_4963_섬의개수.py
@@ -1,8 +1,8 @@
 # 4963번 섬의 개수 
 # swea 스택2 미로 확인용 문제를 참고함 
-# 리커젼 에러 나서 임포트 시스 써서 해결했더니 메모리 초과남;;; 우째 이거;; 
+# 리커젼 에러 나서 임포트 시스 써서 해결했더니 메모리 초과남;;; 우째 이거;; => 대박 ~~~ 
+# 31452kb 80ms 
 import sys
-
 sys.setrecursionlimit(10**6)
 
 di = [0, 1, 1, 1, 0, -1, -1, -1]
@@ -14,8 +14,7 @@ def dfs(r, c):
     for k in range(8):
         nr = r + di[k]
         nc = c + dj[k]
-        if 0 <= nr < h and 0 <= nc < w and arr[nr][nc] == 1 and visited[nr][nc]!= 1: 
-            # 나는 처음에  0 <= nr < w and 0 <= nc < h 했는데 범위 오류나서 바꿔줬더니 맞았어 근데 왜 그런지 모르겠어.. 알려줄 사람 구함   
+        if 0 <= nr < h and 0 <= nc < w and arr[nr][nc] == 1 and visited[nr][nc] == 0: # 헐 맞음 !!!!!!!! 
             dfs(nr,nc)
              
 
