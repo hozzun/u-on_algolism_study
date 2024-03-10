0c0ae3fc6791f6719a0c7133cc4852de77d4db9a
chajoyhoi <156291521+chajoyhoi@users.noreply.github.com>
2024-03-10 18:11:56 +0900

    Create [실2]BOJ_1012_유기농배추.py

---
+++ 차유림/3월/0310/[실2]BOJ_1012_유기농배추.py
@@ -0,0 +1,28 @@
+def dfs(r,c):
+    baechu[r][c] = 2
+    for k in range(4):
+        nr = r + di[k]
+        nc = c + dj[k]
+        if 0 <= nr < M and 0 <= nc < N and baechu[nr][nc] == 0:
+            dfs(nr,nc)
+
+
+T = int(input())
+for tc in range(1, T+1):
+    M, N, K = map(int, input().split())
+    baechu = [[0] * M for _ in range(N)]
+    di = [0, 1, 0, -1]
+    dj = [1, 0, -1, 0]
+    cnt = 0
+    for _ in range(K):
+        y, x = map(int, input().split())        
+        baechu[x][y] = 1
+        
+    
+    for i in range(M):
+        for j in range(N):
+            if baechu[i][j] == 1:
+                dfs(i, j)
+                cnt += 1
+    
+    print(cnt)
