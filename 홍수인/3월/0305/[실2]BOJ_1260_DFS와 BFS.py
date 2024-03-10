ec1b4e69dcc68865553fe2c2602072948ca5d5dc
Suin Hong <78659190+ssuinh@users.noreply.github.com>
2024-03-10 11:23:05 +0900

    Update [실2]BOJ_1260_DFS와 BFS.py

--- 홍수인/3월/0305/[실2]BOJ_1260_DFS와 BFS.py
+++ 홍수인/3월/0305/[실2]BOJ_1260_DFS와 BFS.py
@@ -15,7 +15,7 @@ def bfs(q):
     while Q:
         c = Q.popleft()
         result_bfs.append(c)
-        for w in graph[c]:
+        for w in graph[c]:    # Q에서 꺼낸 노드의 인접 정점이 방문하지 않은 상태라면, Q에 append, 방문표시
             if visited[w] == 0:
                 Q.append(w)
                 visited[w] = 1
@@ -25,19 +25,21 @@ graph = [[] for _ in range(n+1)]
 visited = [0] * (n+1)
 result_dfs = []
 result_bfs = []
-for _ in range(m):
+for _ in range(m):    # 그래프 형성
     a, b = map(int, input().split())
     graph[a].append(b)
-    graph[b].append(a)
+    graph[b].append(a)   # 양방향
 
 for i in graph:   # 숫자가 작은 노드부터 방문하므로 오름차순 정렬.
     i.sort()
 
+# dfs
 dfs(v)
 print(*result_dfs)
 
-visited = [0] * (n+1)
-visited[v] = 1
+# bfs
+visited = [0] * (n+1)    # visited 초기화
+visited[v] = 1         # 시작점 방문표시
 Q = deque([v])
 bfs(v)
 print(*result_bfs)
