4f7d92dc459a57e662eccf412b3c483cb241af6e
chajoyhoi <156291521+chajoyhoi@users.noreply.github.com>
2024-03-14 17:55:37 +0900

    Update hello?.py

--- 김의태/hello?.py
+++ 김의태/hello?.py
@@ -4,3 +4,5 @@ print('can you please add google excel address?')
 
 print('here~')
 print('https://docs.google.com/spreadsheets/d/1spWKk2Ew0Hk35G-mF62iR_wvgnIHTbazKtE4KlVaZec/edit#gid=116792328')
+
+# ㅋㅋㅋㅋㅋ 웃겨ㅜ
