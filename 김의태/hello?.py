de9e3d79c2a545671036f54953f3c32c542a395e
JinAyeong <156287371+JinAyeong@users.noreply.github.com>
2024-03-14 17:53:16 +0900

    Update hello?.py

--- 김의태/hello?.py
+++ 김의태/hello?.py
@@ -1,3 +1,6 @@
 print('hello?')
 print('long time no see~!')
 print('can you please add google excel address?')
+
+print('here~')
+print('https://docs.google.com/spreadsheets/d/1spWKk2Ew0Hk35G-mF62iR_wvgnIHTbazKtE4KlVaZec/edit#gid=116792328')
