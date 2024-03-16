743d613355f982a764bc55b900d0de92c2f11ab4
Suin Hong <78659190+ssuinh@users.noreply.github.com>
2024-03-16 13:51:45 +0900

    Create [실3]BOJ_2108_통계학.py

---
+++ 홍수인/3월/0315/[실3]BOJ_2108_통계학.py
@@ -0,0 +1,35 @@
+n = int(input())
+num_lst = [int(input()) for _ in range(n)]
+
+print(int(sum(num_lst)/n))
+num_lst.sort()
+print(num_lst[n//2])
+
+set_lst = set(num_lst)
+count_lst = []
+
+if len(set_lst) == n:  # 모든 수가 중복되지 않는 경우
+    if len(num_lst) == 1:
+        print(num_lst[0])
+    else:
+        print(num_lst[1])
+else:
+    for i in num_lst:
+        a = num_lst.count(i)
+        if a in count_lst:    # 개수가 똑같은 게 존재하면
+            result = i
+        count_lst.append(a)
+
+    max_val = 0
+    set_l = list(set_lst)
+    set_l.sort()
+    if max(count_lst) == result:  # 개수가 같은 count수가 최대 count이면 i는 최빈값이 여러개인 리스트 중 두번쨰 작은수가 됨.
+        print(i)
+    else:   # 겹치는 count의 수가 최빈값의 갯수가 아닌경우 최대 count 값을 찾아 최빈값 구함.
+        for i in range(len(count_lst)):
+            if count_lst[i] > max_val:
+                max_val = count_lst[i]
+                idx = i
+        print(set_l[idx])
+
+print(num_lst[-1] - num_lst[0])
