29f05827fcc551c81e0ea8a7df4951c81a07018a
JinAyeong <156287371+JinAyeong@users.noreply.github.com>
2024-03-22 14:53:24 +0900

    Create [실1]BOJ_1697_숨바꼭질.py

---
+++ 진아영/3월/0321/[실1]BOJ_1697_숨바꼭질.py
@@ -0,0 +1,36 @@
+# 파이썬   35320 KB, 104 ms, 666 B
+
+
+from collections import deque
+
+def move(i):
+
+    global min_time
+
+    Q = deque()
+    visited = [0] * 100001    # 몇초 걸렸는지 저장
+    Q.append(i)
+    visited[i] = 1
+
+    while Q:
+        cur = Q.popleft()
+
+        if cur == finish:                   # 도착지점에 도착하면
+            if visited[cur] < min_time:     # 최소값 저장하고
+                min_time = visited[cur]
+            return                          # return
+
+        next_c = [cur+1, cur-1, cur*2]    # 다음 이동 리스트
+        for j in next_c:
+            if 0 <= j <= 100000 and not visited[j]:    # 방문하지 않은 곳이고 범위 안에 있으면
+                visited[j] = visited[cur] + 1          # 걸린 시간 저장
+                Q.append(j)
+
+
+
+start, finish = map(int, input().split())
+
+min_time = 999999999999999999999999
+move(start)
+
+print(min_time-1)
