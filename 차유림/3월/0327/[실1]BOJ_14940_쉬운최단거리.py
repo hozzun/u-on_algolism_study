7b383cdef45c352e6ff1069d69be318d52bf059b
chajoyhoi <156291521+chajoyhoi@users.noreply.github.com>
2024-03-27 21:50:52 +0900

    Update [실1]BOJ_14940_쉬운최단거리.py

--- 차유림/3월/0327/[실1]BOJ_14940_쉬운최단거리.py
+++ 차유림/3월/0327/[실1]BOJ_14940_쉬운최단거리.py
@@ -12,9 +12,51 @@ def bfs():
     visited = [[0] * M for _ in range(N)]
     for i in range(N):
         for j in range(M):
+            if arr[i][j] == 2: # 2 인 부분 큐에넣기 
+                q.append((i, j))
+                visited[i][j] = 1 # 방문표시
+
+    while q:
+        r, c = q.popleft()
+        for k in range(4):
+            nr = r + di[k]
+            nc = c + dj[k]
+            if 0 <= nr < N and 0 <= nc < M and arr[nr][nc] != 0 and visited[nr][nc] == 0:
+                arr[nr][nc] = arr[r][c] + 1 # +1로 거리 측정
+                visited[nr][nc] = 1 # 방문표시
+                q.append((nr, nc))           
+    
+    return arr
+
+result = bfs()
+for i in range(N):
+    for j in range(M):
+        if arr[i][j] != 0:
+            arr[i][j] -= 2 # -2 를 해줘서 도달 할 수 없는 곳도 알아서 -1 이 되어버려서 틀리지 않았던 거임;;;!!! 
+
+for row in result:
+    print(*row)
+
+
+# ----------------------------------------------------------------------
+from collections import deque
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+arr = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+di = [0, 1, 0, -1]
+dj = [1, 0, -1, 0]
+
+def bfs():
+    q = deque()
+    visited = [[0] * M for _ in range(N)]
+
+    for i in range(N):
+        for j in range(M):
             if arr[i][j] == 2:
                 q.append((i, j))
                 visited[i][j] = 1
+                arr[i][j] = 0 # 처음 값이 계속 2로 되어있었어!!!! 0으로 안해줘서 !! 아영언니 짱 ! 
 
     while q:
         r, c = q.popleft()
@@ -26,13 +68,13 @@ def bfs():
                 visited[nr][nc] = 1
                 q.append((nr, nc))           
     
-    return arr
+    return visited
 
-result = bfs()
+visited = bfs()
 for i in range(N):
     for j in range(M):
-        if arr[i][j] != 0:
-            arr[i][j] -= 2
+        if arr[i][j] == 1 and visited[i][j] == 0:
+            arr[i][j] = -1 # 원래 갈 수 있는 땅인 부분 중에서 도달 할 수 없으면 -1 출력 !!!!!!! 문제를 잘 읽자..
 
-for row in result:
+for row in arr:
     print(*row)
