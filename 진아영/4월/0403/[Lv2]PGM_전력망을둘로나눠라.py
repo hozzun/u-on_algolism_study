09b0b23fcbce0ae96fadf52991d17d863a5dcd8c
JinAyeong <156287371+JinAyeong@users.noreply.github.com>
2024-04-08 21:33:14 +0900

    Update [Lv2]PGM_전력망을둘로나눠라.py

--- 진아영/4월/0403/[Lv2]PGM_전력망을둘로나눠라.py
+++ 진아영/4월/0403/[Lv2]PGM_전력망을둘로나눠라.py
@@ -14,8 +14,9 @@ def dfs(k):
 def solution(n, wires):
     global line, visited, adjl
     answer = 100
+
     
-    for i in range(n-1):
+    for i in range(n-1):    # 간선 하나씩 없애보면서 완전탐색
         new_wires = copy.deepcopy(wires)
         new_wires.pop(i)
         visited = [0] * (n+1)
@@ -24,15 +25,15 @@ def solution(n, wires):
             a, b = new_wires[w]
             adjl[a].append(b)
             adjl[b].append(a)
-        
-        result = []
-        for j in range(1, n+1):
+
+        result = []               # 전력망 길이(2개)가 들어갈 리스트
+        for j in range(1, n+1):   # 한 간선을 없앴을때 이때의 전력망 구하기
             if not visited[j]:
                 line = 1
                 visited[j] = 1
                 dfs(j)
                 result.append(line)
-        print(result)
+
         if len(result) > 1:
             answer = min(answer, abs(result[0]-result[1]))
     
