ab236f5a55a8d7c6c93932e4e1a244a7dc576d03
JinAyeong <156287371+JinAyeong@users.noreply.github.com>
2024-04-15 22:51:56 +0900

    Update 실패_[실2]BOJ_한줄로서기.py

--- 진아영/4월/0413/실패_[실2]BOJ_한줄로서기.py
+++ 진아영/4월/0413/실패_[실2]BOJ_한줄로서기.py
@@ -1,25 +1,25 @@
-# 완전탐색으로 구했더니 메모리 초과... 흠~ 더 고민해보겠어~~~ 실버2라 뭔가 더 간단한 방법이 있을거같은데~~~~~~~~~~
+# 31120 KB, 44 ms
 
-from itertools import permutations
+N = int(input())
+arr = list(map(int, input().split()))    # 처음 인풋
+result = [0] * N                         # 결과
 
-def check(num):
-    for n in range(2, num):
-        if num % n != 0:
-            continue
-        else:
-            return False
-    return True
-
-def solution(numbers):
-    answer = 0
-    numbers = list(map(str, numbers))
-    candidate = set()
-    for i in range(len(numbers)):
-        lst = list(permutations(numbers, i))
-        for j in lst:
-            candidate.add(int(j))
-    candidate = list(candidate)
-    for k in candidate:
-        if check(k):
-            answer += 1
-    return answer
+for i in range(N):  # 0번 인덱스(1번 사람)부터 돌면서 제자리 찾기
+    cnt = 0
+    if arr[i] == 0:                      # 만약 i번째 사람의 앞쪽에 i보다 키큰사람이 없으면
+        result[result.index(0)] = i+1    # 빈칸 찾아서 그자리 차지하기 (무조건 앞에서부터 차지하기 때문에 index 메서드 사용)
+    else:                                # 그렇지 않으면
+        for j in range(N):               
+            if result[j] == 0:           # 자기보다 키큰사람(빈칸 = 0) 세주기
+                cnt += 1
+            if arr[i] == cnt:            # 자기보다 키큰사람이 조건에 맞을 경우
+                cur = j+1                # j 다음부터 빈칸에 서기 위해 빈칸 찾기
+                while True:
+                    if result[cur]:      # 빈칸이 아닐경우
+                        cur += 1         # 다음칸으로
+                    else:                # 빈칸이면
+                        break            # 브레이크하고
+                result[cur] = i+1        # 그 칸에 들어가기
+                break
+                
+print(*result)
