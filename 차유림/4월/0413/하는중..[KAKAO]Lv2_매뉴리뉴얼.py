ca363efaf5ff4f3d1cc4462c6369ed34c7f02ee7
chajoyhoi <156291521+chajoyhoi@users.noreply.github.com>
2024-04-16 22:46:02 +0900

    Create 하는중..[KAKAO]Lv2_매뉴리뉴얼.py

---
+++ 차유림/4월/0413/하는중..[KAKAO]Lv2_매뉴리뉴얼.py
@@ -0,0 +1,28 @@
+# 메뉴 리뉴얼 
+# 아직 하는 중 ...... 졸려서 자야겟어.
+from itertools import combinations 
+# def solution(orders, course):
+#     answer = []
+#     for i in course:
+#         comb = []
+#         for j in orders:
+#             comb += list(combinations(j, i))
+
+#     return answer
+
+orders = ["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"]
+course = [2, 3, 4]
+menu = {}
+comb = []
+for i in course:
+    for j in orders:
+        comb += list(combinations(sorted(j), i))
+
+for i in comb:
+    menu_val = ''.join(i)
+
+    if menu_val in menu:
+        menu[menu_val] += 1
+    else:
+        menu[menu_val] = 1
+    
