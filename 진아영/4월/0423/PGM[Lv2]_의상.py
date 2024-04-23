3b469c166d16a41dabaf6a286225bb2d0f8727b2
JinAyeong <156287371+JinAyeong@users.noreply.github.com>
2024-04-23 23:23:03 +0900

    Update PGM[Lv2]_의상.py

--- 진아영/4월/0423/PGM[Lv2]_의상.py
+++ 진아영/4월/0423/PGM[Lv2]_의상.py
@@ -1,4 +1,27 @@
-# 96.4점 1번 시간초과... 내일은 여기서 함수를 빼봐야지....
+# 버전2. 함수 빼고 for 문으로 변경~
+
+def solution(clothes):
+    global answer, categories, closet
+    
+    answer = 1
+    closet = {}
+
+    for cloth, category in clothes:
+        if category in closet:
+            closet[category] += 1
+        else:
+            closet[category] = 1
+            
+    cnts = closet.values()
+    
+    for cnt in cnts:
+        answer *= (cnt + 1)   # 안입는경우 ~ 옷고르는 경우까지 모든 경우의 수 곱한다
+
+    return answer - 1
+
+#-------------------------------------------------------------------------
+
+# 버전1. 96.4점 1번 시간초과... 내일은 여기서 함수를 빼봐야지....
 
 def wear(idx, case):
     global answer, categories, closet
@@ -7,10 +30,10 @@ def wear(idx, case):
         answer += case
         return
 
-    # 현재 인덱스 선택
+    # 현재 인덱스 중 하나 골라서 입는 경우의 수 곱해서 넘김
     wear(idx+1, case * closet[categories[idx]])
 
-    # 현재 인덱스 선택하지 않음
+    # 현재 인덱스 안입는경우
     wear(idx+1, case)
 
 def solution(clothes):
