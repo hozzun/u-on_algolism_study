bd694a126c6c20178e41769b07b90affe8b02923
hojun-kim <156287257+ssafy11@users.noreply.github.com>
2024-04-24 23:36:11 +0900

    Create 저장[골5]BOJ_6198_옥상 정원 꾸미기.py

---
+++ 김호준/4월/0419/저장[골5]BOJ_6198_옥상 정원 꾸미기.py
@@ -0,0 +1,35 @@
+# ㅈㅅㅈㅅ 다시 못풀어봄..
+
+from collections import deque
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+N = int(input())
+
+arr = deque()
+for _ in range(N):
+    h = int(input())
+    arr.append(h)
+
+answer = 0
+while True:
+    if len(arr) == 1:
+        # answer += count
+        break
+
+    count = 0
+
+    check = arr.popleft()
+    for i in range(len(arr)):
+        if check > arr[i]:
+            count += 1
+            if i == len(arr) - 1:
+                answer += count
+                break
+
+        elif check <= arr[i]:
+            answer += count
+            break
+
+print(answer)
