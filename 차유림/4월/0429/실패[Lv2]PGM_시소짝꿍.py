a713d7600a9b30b0eced371e043b401209f35eb8
chajoyhoi <156291521+chajoyhoi@users.noreply.github.com>
2024-05-02 19:56:17 +0900

    Update 실패[Lv2]PGM_시소짝꿍.py

--- 차유림/4월/0429/실패[Lv2]PGM_시소짝꿍.py
+++ 차유림/4월/0429/실패[Lv2]PGM_시소짝꿍.py
@@ -1,3 +1,4 @@
+# 실패 버전 .. ㅠㅠ! 
 from itertools import combinations
 def solution(weights):
     weights.sort()
@@ -9,4 +10,18 @@ def solution(weights):
         if val in see:
             jjack += 1
         
+    return jjack
+# -------------- 딕셔너리 사용하라는 힌트 듣고 해보는 버전..
+def solution(weights):
+    weights.sort()
+    seesaw = [1, 2, 3, 4, 3/2, 4/3]
+    cnt = 0
+    jjack = {}
+    for i in weights:
+        if i in jjack:
+            jjack[i] += 1
+        else:
+            jjack[i] = 1
+    # for i in jjack:
+    #     if 
     return jjack
