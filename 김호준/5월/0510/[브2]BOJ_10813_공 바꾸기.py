00b900555a3704ee957548a8dc227dc3f5033753
hojun-kim <156287257+ssafy11@users.noreply.github.com>
2024-05-29 15:36:15 +0900

    Create [브2]BOJ_10813_공 바꾸기.py

---
+++ 김호준/5월/0510/[브2]BOJ_10813_공 바꾸기.py
@@ -0,0 +1,11 @@
+N, M = map(int, input().split())
+
+arr = list(range(N+1))
+
+for _ in range(M):
+    i, j = map(int, input().split())
+    arr[i], arr[j] = arr[j], arr[i]
+    
+print(*arr[1:])
+
+# 1월달에 풀었었네 ㅎㅎ..
